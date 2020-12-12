7fbbaba8aa5a09cfac5ade4661cc80723f5fe022
Jihye Woo <woojihye2339@gmail.com>
2020-12-12 17:28:15 +0900

    feat : init Cube and Commend class   - init Cube class for easily manipulating the cube info   - init Commend enum class for different commend input

---
+++ src/com/test/code_squad/Commend.java
@@ -0,0 +1,54 @@
+package com.test.code_squad;
+
+public enum Commend {
+    U, R, L, B;
+
+    private int x1, x2;
+    private int y1, y2;
+    private int z1, z2;
+
+    private int sign;
+
+    private Direction direction;
+
+    static {
+        U.x1 = 0; U.x2 = 0; U.y1 = 0; U.y2 = 1; U.z1 = 0; U.z2 = 2;
+        R.x1 = 0; R.x2 = 2; R.y1 = 1; R.y2 = 2; R.z1 = 2; R.z2 = 2;
+        L.x1 = 0; L.x2 = 0; L.y1 = 1; L.y2 = 0; L.z1 = 2; L.z2 = 0;
+        B.x1 = 2; B.x2 = 0; B.y1 = 2; B.y2 = 1; B.z1 = 2; B.z2 = 2;
+
+        U.sign = R.sign = 1;
+        L.sign = B.sign = -1;
+    }
+
+    static Commend is(String commend) {
+        switch (commend) {
+            case "U":
+                return U;
+            case "R":
+                return R;
+            case "L":
+                return L;
+            case "B":
+                return B;
+            default:
+                throw new IllegalArgumentException("Unexpected commend value: " + commend);
+        }
+    }
+
+    public int getSign() {
+        return sign;
+    }
+
+    public int x1() { return x1; }
+
+    public int x2() { return x2; }
+
+    public int y1() { return y1; }
+
+    public int y2() { return y2; }
+
+    public int z1() { return z1; }
+
+    public int z2() { return z2; }
+}
---
+++ src/com/test/code_squad/Cube.java
@@ -0,0 +1,70 @@
+package com.test.code_squad;
+
+import java.util.Arrays;
+
+public class Cube {
+
+    private char[][] cube;
+
+    private final int CUBE_LENGTH = 3;
+
+    public Cube(char[][] cube) {
+        this.cube = cube;
+    }
+
+    // shift starting point
+    public void shift(Commend c, String direction){
+        // get target word and create shiftedString instance
+        ShiftedString shiftedString = new ShiftedString(getWord(c));
+        // get N from commend c / get direction from isLeft
+
+        // create shiftInfo instance
+        shiftedString.shifting(c.getSign(), direction);
+
+        // update cube info
+        update(shiftedString.getWord(), c);
+    }
+
+    // create target word
+    public String getWord(Commend c) {
+        return new StringBuilder()
+                .append(cube[c.x1()][c.x2()])
+                .append(cube[c.y1()][c.y2()])
+                .append(cube[c.z1()][c.z2()])
+                .toString();
+    }
+
+    // update cube
+    public void update(String shifted, Commend c) {
+        cube[c.x1()][c.x2()] = shifted.charAt(0);
+        cube[c.y1()][c.y2()] = shifted.charAt(1);
+        cube[c.z1()][c.z2()] = shifted.charAt(2);
+    }
+
+
+    // print cube
+    public void print() {
+        System.out.println(cube.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Cube{" +
+                "cube=" + Arrays.toString(cube) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cube cube1 = (Cube) o;
+        // comparing contents of the cube
+        return Arrays.equals(cube, cube1.cube);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(cube);
+    }
+}
